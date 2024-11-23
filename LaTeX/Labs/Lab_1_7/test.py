4e1a6d00fe4c31b8806b62ce9f4f9a08bc12932c
Zeny-star <eturchanin@gmail.com>
2024-11-23 08:05:06 +0300

    math_lab

--- LaTeX/Labs/Lab_1_7/test.py
+++ LaTeX/Labs/Lab_1_7/test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy
 
 
 h1=[32.0/100, 31.1/100, 31.5/100, 31.5/100, 31.2/100]
@@ -29,11 +28,13 @@ delta_h1 = np.std(h1) / np.sqrt(len(h1))
 A = g * t_mean**2 * h1_mean / (h * (h + h1_mean)) - 1
 delta_A = A * np.sqrt((2 * delta_t / t_mean)**2 + (delta_h / h)**2 + (delta_h1 / h1_mean)**2)
 delta_I = I_2 * np.sqrt((delta_m / m)**2 + (2 * delta / r)**2 + (delta_A / A)**2)
-I_theory=m*R**2/2
+m_diska=m*(x*np.pi*R**2/(l*np.pi*r**2+x*np.pi*R**2))
+I_theory=m_diska*R**2/2
 print(f"Среднее время t_mean: {t_mean:.4f} с")
 print(f"Средняя высота h1_mean: {h1_mean:.4f} м")
 print(f"Момент инерции I_1 по формуле (6): {I_1} кг·м²")
 print(f"Момент инерции I_2 по формуле (11): {I_2} кг·м²")
-print(f"Момент инерции I_theory по формуле: {I_theory/2} кг·м²")
+print(f"Момент инерции I_theory по формуле: {I_theory} кг·м²")
 print(f"Погрешность для I_2: {delta_I} кг·м²")
 
+print(m, m_diska)
--- Python/test.py
+++ Python/test.py
@@ -1,17 +1,44 @@
-#на вход строка  и список слов, вернуть словарь где в ключ - эл-т списка
-words = ["apples"," ", "0"]
-phrase = "John likes apples"
-dict_of_words = {}
-def foo(words, phrase):
-    for i in words:
-        if i in phrase:
-            dict_of_words[i] = phrase.find(i)
-        else:
-            dict_of_words[i] = -1
-    return dict_of_words
-print(foo(words, phrase))
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Начальные значения
+c_new = 1.0  # Начальное значение для c_x
+a_new = 2.0  # Начальное значение для a_x
 
+# Количество точек, которые нужно сгенерировать
+num_points = 20
 
+# Списки для хранения значений c_x и a_x
+c_values = [1.0]
+a_values = [2.0]
 
+# Генерация следующих точек
+for i in range(1, num_points):
+    # Вычисляем новое значение a_x
+    a_new = a_new - (a_new * c_new) / 2
+    print(a_new)
+    a_values.append(a_new)
+    # Вычисляем новое значение c_x
+    c_new = (c_new ** 2 * (c_new - 3)) / 4
+    c_values.append(c_new)
+# Подготовка данных для построения графика
+x_values = np.arange(num_points)  # Ось X будет просто индексами точек
 
+# Построение графика
+plt.plot(x_values, a_values, marker='o', linestyle='-', color='blue', label='График a_x')
+plt.title('График a_x в зависимости от индекса')
+plt.xlabel('Индекс (n)')
+plt.ylabel('a_x')
+plt.axhline(0, color='black', linewidth=0.5, ls='--')
+plt.axvline(0, color='black', linewidth=0.5, ls='--')
+plt.grid(color='gray', linestyle='--', linewidth=0.5)
+plt.legend()
+
+# Выводим все точки на график
+# for index, value in enumerate(a_values):
+#  plt.text(x_values[index], value, f'({x_values[index]}, {value:.2f})')
+
+# Настройки отображения осей
+plt.xlim(0, num_points - 1)
+plt.ylim(1, 2)  # Устанавливаем пределы оси Y от -1 до 1
+plt.show()
